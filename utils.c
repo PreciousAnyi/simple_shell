016ce34c6f0caf957a10a441e021abde47425525
preciousanyi <jkpresh@gmail.com>
2023-12-14 10:30:41 +0100

    fixed utils.c

--- utils.c
+++ utils.c
@@ -53,7 +53,7 @@ size_t _strlen(const char *string)
  */
 int _strcmp(const char *s1, const char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s1)
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
 	{
 		s1++;
 		s2++;
